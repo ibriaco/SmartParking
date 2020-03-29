045c73b6ab411de54b6c0486a06f62fd98f0aaea
gregalletti <gregorio.galletti@mail.polimi.it>
2020-03-29 21:38:05 +0200

    deleted some useless files

--- smartparking/actions/actions.js
+++
@@ -1,9 +0,0 @@
-import { ACTION_UPDATE_AREAS, ACTION_UPDATE_FILTERS } from '../constants/redux_constants';
-
-export function updateAreas(areas) {
-    return {
-        type: ACTION_UPDATE_AREAS,
-        payload: areas
-    }
-}
-
