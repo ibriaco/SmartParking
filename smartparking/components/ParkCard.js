1357679124bd74d766f22121e245f8bee3223fcf
gregalletti <gregorio.galletti@mail.polimi.it>
2020-03-31 18:41:26 +0200

    Parking list added

---
+++ smartparking/components/ParkCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+
+export default class ParkCard extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+
+    return (
+    <View>
+    <View
+      style={{
+        width: "100%",
+        padding: 16,
+        backgroundColor: "rgb(255, 255, 255)"
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 12,
+          marginBottom: 5,
+          fontWeight: "bold",
+          color: "rgba(0, 0, 0, 0.5)"
+        }}
+      >
+        {this.props.item.id}
+      </Text>
+      <Text
+        style={{
+          fontSize: 26,
+          maxWidth: "60%",
+          fontWeight: "bold",
+          color: "rgb(51, 51, 51)"
+        }}
+      >
+        Latitude and Longitude: {this.props.item.lat} {this.props.item.lon}
+      </Text>
+    </View>
+  </View>
+);}}
--- smartparking/screens/ListScreen.js
+++ smartparking/screens/ListScreen.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
+import { connect } from 'react-redux';
+import ParkCard from "../components/ParkCard";
+import CardList from "react-native-card-animated-modal";
 
-export default class ListScreen extends Component {
+
+class ListScreen extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      parkCards: []
+    }
 
   }
 
   componentDidMount(){
-    console.log("moooooounttt")
+    const desiredNumberOfObjects = 2;
+    let cards = [];
+
+    for(let i = 0; i < desiredNumberOfObjects; i++) {
+      console.log("Area: " + this.props.areas[i].id + this.props.areas[i].latitude+ this.props.areas[i].longitude )
+      cards[i] = {
+        id: this.props.areas[i].id,
+        lat: this.props.areas[i].latitude,
+        lon: this.props.areas[i].longitude,
+        image: {
+          uri: "https://www.venetoformazione.it/wp-content/uploads/2016/12/ottimizzare-immagini-display-retina.jpg"
+        },
+        renderItem: ({ item }) => <ParkCard item={item}/>
+  };
+
+  this.setState({parkCards: cards})
+
+  console.log(cards)
+
+}
+    
   }
-
-
     render() {
-
         return (
-          <View style={styles.container}>
-            
-            <Text>{this.props.navigation.getParam()}</Text>
-          
-          </View>
+          <CardList    
+          data={this.state.parkCards}
+          renderItem={({ item, index }) => {
+            //Render card per item
+            if (item.renderItem) return item.renderItem({ item, index });
+
+            //Default card when not specified 
+            return (
+              <View>
+                <Text>Default Content</Text>
+              </View>
+            );
+          }}
+          renderDetails={({ item, index }) => (
+            <View style={{ paddingVertical: 30, paddingHorizontal: 16 }}>
+              <Text style={{ color: "rgba(0, 0, 0, 0.7)", fontSize: 18 }}>
+                Id Area: {item.id}
+              </Text>
+            </View>
+          )}
+        />
         )
     }
 }
@@ -46,3 +86,21 @@ const styles = StyleSheet.create({
       left: 50
     }
   });
+
+  function mapStateToProps(state) {
+    return {
+      //state.areas gets data from the store
+      //and we are mapping that data to the prop named areas
+      areas: state.areas, 
+      tappedArea: state.tappedArea
+    }
+  }
+  
+  function mapDispatchToProps(dispatch) {
+    return {
+      updateArea: (param) => dispatch({type: "UPDATE_AREA", param: param}), 
+      updateTappedArea: (param) => dispatch({type: "UPDATE_TAPPED_AREA", param: param}),    
+    }
+  }
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(ListScreen);
